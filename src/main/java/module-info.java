4a0c0cec634033ae55e4d7f27bd26302c42b78d6
plukesch <patrick.lukesch@chello.at>
2024-05-07 15:54:35 +0200

    fixed Database/ Dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,15 +3,17 @@ module at.ac.fhcampuswien.fhmdb {
     requires javafx.fxml;
 
     requires com.jfoenix;
-    requires okhttp3;
     requires com.google.gson;
-//    requires ormlite.core;
-//    requires ormlite.jdbc;
+    requires okhttp3;
+    requires ormlite.jdbc;
     requires java.sql;
 
     opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
+    exports at.ac.fhcampuswien.fhmdb.database to ormlite.jdbc;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     exports at.ac.fhcampuswien.fhmdb.models;
     exports at.ac.fhcampuswien.fhmdb;
+
+    opens at.ac.fhcampuswien.fhmdb.database to ormlite.jdbc;
 }
